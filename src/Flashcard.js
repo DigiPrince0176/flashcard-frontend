b5ac047fbc75448af25e54bf76ec058d23abe286
Prince Shah <prince.shah@LGISERVERS.COM>
2026-04-03 14:37:56 +0530

    back card test 3

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -24,7 +24,7 @@ const Flashcard = ({ question, answer, direction }) => {
           <div
   className="card-back"
   style={{
-    maxHeight: "600px",
+    maxHeight: "550px",
     overflowY: "auto",
     padding: "20px",
     display: "flex",

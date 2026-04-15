97b116bf21300f1b675ae93f2d2c49ef611f0de3
DigiPrince0176 <digitalinfinity.ldpl@gmail.com>
2026-04-15 15:29:04 +0530

    Update Flashcard component layout styles
    Refactor card-back style to use flex display and improve layout.

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -9,61 +9,58 @@ const Flashcard = ({ question, answer, direction }) => {
     setFlipped(false);
   }, [question]);
 
-  return (
-    <div className={`card-slide ${direction}`}>
-      <div className={`card ${flipped ? "flipped" : ""}`}>
-        <div className="card-inner">
+ return (
+  <div className={`card-slide ${direction}`}>
+    <div className={`card ${flipped ? "flipped" : ""}`}>
+      <div className="card-inner">
 
-          <div className="card-front">
-            <p>{question}</p>
-            <button onClick={() => setFlipped(true)}>
-              Show Answer
-            </button>
-          </div>
+        {/* FRONT */}
+        <div className="card-front">
+          <p>{question}</p>
+          <button onClick={() => setFlipped(true)}>
+            Show Answer
+          </button>
+        </div>
 
-          <div
-  className="card-back"
-  style={{
-    maxHeight: "500px",
-    overflowY: "auto",
-    padding: "20px",
-    display: "block",
-  }}
->
-
-          <div
-              style={{
-                   display: "flex",
-                   flexDirection: "column",
-                   height: "100%",   // important
-                  }}
-              >
+        {/* BACK */}
+        <div
+          className="card-back"
+          style={{
+            maxHeight: "500px",
+            overflowY: "auto",
+            padding: "20px",
+            display: "flex",           // 👈 changed
+            flexDirection: "column",   // 👈 important
+          }}
+        >
           <div
             style={{
-                 wordWrap: "break-word",
-                 overflowWrap: "break-word",
-                 lineHeight: "1.6",
-                 marginTop: "10px",
-                 fontSize: "22px",
-                 color: "white",
-                flexGrow: 1   // 👈 pushes button down
-                }}
-         dangerouslySetInnerHTML={{ __html: answer || "" }}
+              wordWrap: "break-word",
+              overflowWrap: "break-word",
+              lineHeight: "1.6",
+              marginTop: "10px",
+              fontSize: "22px",
+              color: "white",
+              flexGrow: 1, // pushes button down
+            }}
+            dangerouslySetInnerHTML={{ __html: answer || "" }}
           />
 
           <button
-             onClick={() => setFlipped(false)}
-               style={{
-                        marginTop: "auto",   // 👈 key line
-                       alignSelf: "center", // optional (center button)
-                    }}
-                >
-                Back
-              </button>
+            onClick={() => setFlipped(false)}
+            style={{
+              marginTop: "auto",
+              alignSelf: "center",
+            }}
+          >
+            Back
+          </button>
         </div>
+
       </div>
     </div>
-  );
+  </div>
+);
 };
 
 export default Flashcard;
